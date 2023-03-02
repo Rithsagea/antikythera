874b7bcc3df12d3c8acbe82411d1e1037b975164
Rithsagea <rithsagea@gmail.com>
2023-03-02 16:09:46 -0500

    longestSubstring

--- leetcode/0003_longestSubstring.cpp
+++ leetcode/0003_longestSubstring.cpp
@@ -1,1 +1,19 @@
 #include "base.hpp"
+
+class Solution {
+public:
+  int lengthOfLongestSubstring(string s) {
+    vector<bool> characters(numeric_limits<unsigned char>::max(), false);
+    int start = 0, end = 0, longest = 0;
+    for (end = 0; end < s.size(); end++) {
+      while (characters[s[end]]) {
+        characters[s[start]] = false;
+        start++;
+      }
+      characters[s[end]] = true;
+      longest = max(longest, end - start + 1);
+    }
+
+    return longest;
+  }
+};
