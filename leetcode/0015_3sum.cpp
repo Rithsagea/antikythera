c3b6f0e2378b408dacad1302c26eb8c7417be321
Rithsagea <rithsagea@gmail.com>
2023-03-24 08:54:17 -0400

    completed 3 sum

--- leetcode/0015_3sum.cpp
+++ leetcode/0015_3sum.cpp
@@ -1,7 +1,5 @@
 #include "base.hpp"
 
-
-
 // Naive Solution
 // class Solution {
 //   public:
@@ -26,3 +24,43 @@
 //   }
 // };
 
+/*
+range i[0, nums.length - 1]
+  skip i if it is duplicate
+  use 2 pointers j at i + 1, k at nums.length - 1
+  while j and k do not pass
+    check nums[i, j, k] sum to 0
+    add to result if it does
+    move j and k to next position (skip duplicates)
+*/
+class Solution {
+  public:
+  vector<vector<int>> threeSum(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> result;
+
+    int sum;
+    for (int i = 0; i < nums.size(); i++) {
+      if(i != 0 && nums[i] == nums[i - 1]) continue;
+
+      int j = i + 1;
+      int k = nums.size() - 1;
+
+      while (j < k) {
+        sum = nums[i] + nums[j] + nums[k];
+        if (sum == 0) {
+          result.push_back({ nums[i], nums[j], nums[k] });
+          while(j < k && nums[j] == nums[j + 1]) j++;
+          while(j < k && nums[k] == nums[k - 1]) k--;
+          j++, k--;
+        } else if (sum > 0) {
+          k--;
+        } else {
+          j++;
+        }
+      }
+    }
+
+    return result;
+  }
+};
